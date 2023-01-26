995367ecc6989be266f7175df48063ab6f7e84ad
Ira Iosub <iosubi@login000.camp.thecrick.org>
2023-01-26 14:27:45 +0000

    removed last ggeasy

--- bin/riboseq_qc.R
+++ bin/riboseq_qc.R
@@ -77,7 +77,8 @@ riboseq_info <- get_info_from_bam(opt$bam, opt$transcript_info)
 p1 <- ggplot(riboseq_info$frame, aes(x = read_length, y = n, fill=factor(frame))) +
     geom_bar(stat="identity", position="dodge") +
     theme_classic() +
-    ggeasy::easy_add_legend_title("Frame") +
+    scale_fill_discrete(name = "Frame")
+    # ggeasy::easy_add_legend_title("Frame") +
     xlim(NA, 40)
 
 p2 <- ggplot(riboseq_info$start_dist %>% filter(rl > 18 & rl < 40), 
