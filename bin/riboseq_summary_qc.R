a5e1b90174a5d2bec1ce02843ccd66cfb7ba7873
Ira Iosub <iosubi@login001.camp.thecrick.org>
2023-01-31 13:11:08 +0000

    fixed input

--- bin/riboseq_summary_qc.R
+++ bin/riboseq_summary_qc.R
@@ -8,7 +8,7 @@ suppressPackageStartupMessages(library(patchwork))
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(optparse))
 
-option_list <- list(make_option(c("-i", "--input_list"), action = "store", type = "character", default=NA, help = "qc summary directory"))
+option_list <- list(make_option(c("-i", "--input_list"), action = "store", type = "character", default=NA, help = "list of space separated qc summary tables"))
 opt_parser = OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
@@ -18,7 +18,9 @@ opt <- parse_args(opt_parser)
 # =========
 
 # Summary files produced by riboseq_qc.R
-summary_df.ls <- list(opt$input_list)
+summary_df.ls <- as.list(strsplit(opt$input_list, " ")[[1]])
+
+# summary_df.ls <- list(opt$input_list)
 full_summary.df <- rbindlist(lapply(summary_df.ls, fread), use.names = TRUE)
 
 
@@ -49,7 +51,6 @@ use <- ggplot(full_summary.df, aes(x = name, y = y, fill = name)) +
   theme(legend.position = "none")
 
 use | exp | dup
-
 
 fwrite(full_summary.df, "qc_summary.tsv.gz", sep = "\t")
 ggsave("qc_summary.pdf", dpi = 300, height = 12,
