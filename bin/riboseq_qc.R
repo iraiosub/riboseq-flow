81193bcc50175c830740b9e16c2100faffd80522
Ira Iosub <iosubi@login000.camp.thecrick.org>
2023-02-01 19:36:38 +0000

    fixed typo

--- bin/riboseq_qc.R
+++ bin/riboseq_qc.R
@@ -174,7 +174,7 @@ if (!is.null(opt$after_premap)) {
     geom_rect(xmin = 26, xmax = 31, alpha = .1,fill = "black")
 
   premapping_plot <- ggplot(mapping_df2 %>% filter(length < 50) %>%
-    dplyr::filter(name != "before_dedup_bam"), aes(as.numeric(x) = length, y = value, fill = name)) +
+    dplyr::filter(name != "before_dedup_bam"), aes(as.numeric(length), y = value, fill = name)) +
     geom_bar(stat="identity", position="dodge") +
     scale_fill_manual(values = name_colours) +
     theme_classic() +
