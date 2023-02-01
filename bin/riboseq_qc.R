61be27f9200807f01581e936f91926d1f74eb479
Ira Iosub <iosubi@login000.camp.thecrick.org>
2023-02-01 19:23:31 +0000

    edit

--- bin/riboseq_qc.R
+++ bin/riboseq_qc.R
@@ -171,8 +171,7 @@ if (!is.null(opt$after_premap)) {
     # ggeasy::easy_remove_legend() +
     scale_y_log10() +
     expand_limits(y=1) +
-    annotate("rect", xmin = 26, xmax = 31, ymin = -Inf, ymax = +Inf,
-           alpha = .1,fill = "grey")
+    annotate("rect", xmin = 26, xmax = 31, alpha = .1,fill = "grey")
 
   premapping_plot <- ggplot(mapping_df2 %>% filter(length < 50) %>%
     dplyr::filter(name != "before_dedup_bam"), aes(x = length, y = value, fill = name)) +
@@ -185,8 +184,7 @@ if (!is.null(opt$after_premap)) {
     theme(legend.position = "none") +
     scale_y_log10()+
     expand_limits(y=1) +
-    annotate("rect", xmin = 26, xmax = 31, ymin = -Inf, ymax = +Inf,
-           alpha = .1,fill = "grey")
+    annotate("rect", xmin = 26, xmax = 31, alpha = .1,fill = "grey")
 
   # ggplot(mapping_df %>% filter(length<50), aes(x = length, y = value, fill = name)) +
   #   geom_bar(stat="identity", position="dodge")
