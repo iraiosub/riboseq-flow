84590769b92eaf50e6b54d3525f64a3e9c5e9e84
Ira Iosub <iosubi@login002.nemo.thecrick.org>
2023-03-22 12:39:07 +0000

    added missing fai parameter

--- run.sh
+++ run.sh
@@ -16,9 +16,15 @@ export NXF_SINGULARITY_CACHEDIR=/nemo/lab/ulej/home/users/luscomben/users/iosubi
 # export  NXF_CONDA_CACHEDIR=/nemo/lab/ulej/home/users/luscomben/users/iosubi/nfcore/riboseq
 export NXF_HOME=/nemo/lab/ulej/home/users/luscomben/users/iosubi/.nextflow # for Nextflow versions > 22.x
 
-nextflow pull iraiosub/riboseq -r dev
+# nextflow pull iraiosub/riboseq -r dev
 
 nextflow run iraiosub/riboseq -r dev \
 -profile crick,singularity,test \
 --input /camp/lab/ulej/home/shared/riboseq_workshop/samplesheet.csv \
 -resume
+
+
+# nextflow run main.nf \
+# -profile crick,singularity,test \
+# --input /camp/lab/ulej/home/shared/riboseq_workshop/samplesheet.csv \
+# -resume
