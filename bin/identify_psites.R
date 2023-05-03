b9693c2c6d045b383c55d098093b55542bc90ff8
Ira Iosub <iosubi@login002.nemo.thecrick.org>
2023-05-03 21:30:20 +0100

    replaced scale

--- bin/identify_psites.R
+++ bin/identify_psites.R
@@ -318,7 +318,9 @@ ggplot2::ggsave(paste0(getwd(), "/ribowaltz_qc/psite_region.pdf"), psite_region.
 # frame_psite_length analyses all read lengths separately and generates a heatmap for each transcript region, while frame_psite processes all reads at once, returning three bar plots.
 
 frames_stratified <- frame_psite_length(filtered_psite.ls, region = "all", length_range = "all")
-frames_stratified.gg <- frames_stratified$plot
+frames_stratified.gg <- frames_stratified$plot +
+  scale_y_continuous(limits = c(minl - 0.5, maxl + 0.5), breaks = seq(min(minl + ((minl) %% 2), maxl), max(minl + ((minl) %% 2), maxl), 
+                    by = max(2, floor((maxl - minl) / 7))))
 
 ggplot2::ggsave(paste0(getwd(), "/ribowaltz_qc/frames_stratified.pdf"), frames_stratified.gg, dpi = 400, height = 12 , width = 10)
 
