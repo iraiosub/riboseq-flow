b4f330cd487712fdab0a9af0cdbb8e7d10cc0adb
Ira Iosub <iosubi@login002.nemo.thecrick.org>
2023-08-04 16:40:34 +0100

    module file not found problem

--- full_test_run.sh
+++ full_test_run.sh
@@ -6,17 +6,20 @@
 #SBATCH --partition=cpu
 
 module purge
+# ml Java/11.0.2
 ml Nextflow/21.10.3
+#ml Nextflow/22.10.3
+#ml Nextflow/23.04.2
 ml Singularity/3.6.4
-ml Graphviz/2.38.0-foss-2016b
+# ml Graphviz/2.38.0-foss-2016b
+ml Graphviz/2.47.2-GCCcore-10.3.0
 
 export NXF_SINGULARITY_CACHEDIR=/nemo/lab/ulej/home/shared/singularity
 export NXF_HOME=/nemo/lab/ulej/home/users/luscomben/users/iosubi/.nextflow
 
-nextflow pull iraiosub/riboseq -r main
+nextflow pull iraiosub/riboseq -r dev-nf
 
-nextflow run iraiosub/riboseq -r main \
-
+nextflow run iraiosub/riboseq -r dev-nf \
 -profile crick \
 -resume \
 --skip_umi_extract \
@@ -26,4 +29,4 @@ nextflow run iraiosub/riboseq -r main \
 --strandedness forward \
 --org GRCh38 \
 --input ./data/samplesheet.csv \
---outdir results_full_test
+--outdir results_full_test_dev_nf
