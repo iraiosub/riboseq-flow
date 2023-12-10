bf61e37513e73fe1a705ea8bc40c7b81a99bf985
Ira Iosub <iosubi@login002.nemo.thecrick.org>
2023-12-10 18:24:49 +0000

    added yeast genome

--- full_test_run.sh
+++ full_test_run.sh
@@ -17,9 +17,9 @@ ml Singularity/3.6.4
 export NXF_SINGULARITY_CACHEDIR=/nemo/lab/ulej/home/shared/singularity
 export NXF_HOME=/nemo/lab/ulej/home/users/luscomben/users/iosubi/.nextflow
 
-nextflow pull iraiosub/riboseq -r dev
+nextflow pull iraiosub/riboseq-flow -r main
 
-nextflow run iraiosub/riboseq -r dev \
+nextflow run iraiosub/riboseq-flow -r main \
 -profile full_test,singularity,crick \
 -resume \
 --outdir results_full_test
